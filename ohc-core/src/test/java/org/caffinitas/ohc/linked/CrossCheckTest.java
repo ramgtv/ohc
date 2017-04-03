dbc3685eb9395f4db937def7edb40b8cb4cc1fc7
Robert Stupp <snazy@snazy.de>
2017-04-03 13:58:00 +0200

    make unit tests more resilient against varying test hardware

--- ohc-core/src/test/java/org/caffinitas/ohc/linked/CrossCheckTest.java
+++ ohc-core/src/test/java/org/caffinitas/ohc/linked/CrossCheckTest.java
@@ -79,6 +79,9 @@ public class CrossCheckTest
             builder.hashTableSize(hashTableSize);
         if (segments > 0)
             builder.segmentCount(segments);
+        else
+            // use 16 segments by default to prevent differing test behaviour on varying test hardware
+            builder.segmentCount(16);
         if (maxEntrySize > 0)
             builder.maxEntrySize(maxEntrySize);
 
